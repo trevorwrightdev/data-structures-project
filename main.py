6a73d9c9a59ce40f97e498c8ae80d4216ec1ff96
trevdev <trevorwrightdev@gmail.com>
2024-02-14 10:27:26 -0800

    added csv reading

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from hashmap import HashMap
+from package import Package
+import csv
+
+def csv_to_array(file_path):
+    data = []
+    with open(file_path, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
+def get_package_map():
+    package_csv_data = csv_to_array("packages.csv")
+    # Initializing the hashmap with 40 elements, one for each package. 
+    map = HashMap(40)
+
+    for package in package_csv_data:
+        package_id = int(package['id'])
+        delivery_address = package['delivery_address']
+        deadline = package['deadline']
+        city = package['city']
+        zipcode = int(package['zip'])
+        weight = int(package['weight'])
+        new_package = Package(package_id, delivery_address, deadline, city, zipcode, weight)
+        map.insert(package_id, new_package)
+    return map
 
 def main():
-    packages = HashMap(40)
+    packages = get_package_map()
+    print(packages.get(1))
 
 
 main()
