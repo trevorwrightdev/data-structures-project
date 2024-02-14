2a760f5a3dfcd1f9997b9a10391e69b8db4683b7
trevdev <trevorwrightdev@gmail.com>
2024-02-14 09:32:04 -0800

    Added hashmap implementation

---
+++ hashmap.py
@@ -0,0 +1,64 @@
+class HashMap:
+ 
+    def __init__(self, size):
+        self.size = size
+        self.hash_table = self.create_buckets()
+ 
+    def create_buckets(self):
+        return [[] for _ in range(self.size)]
+ 
+    def insert(self, key, val):
+       
+        hashed_key = hash(key) % self.size
+         
+        bucket = self.hash_table[hashed_key]
+ 
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+             
+            if record_key == key:
+                found_key = True
+                break
+ 
+        if found_key:
+            bucket[index] = (key, val)
+        else:
+            bucket.append((key, val))
+ 
+    def get(self, key):
+       
+        hashed_key = hash(key) % self.size
+         
+        bucket = self.hash_table[hashed_key]
+ 
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+             
+            if record_key == key:
+                found_key = True
+                break
+ 
+        if found_key:
+            return record_val
+        else:
+            return "No record found"
+ 
+    def delete_val(self, key):
+       
+        hashed_key = hash(key) % self.size
+         
+        bucket = self.hash_table[hashed_key]
+ 
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+             
+            if record_key == key:
+                found_key = True
+                break
+        if found_key:
+            bucket.pop(index)
+        return
+ 
---
+++ main.py
@@ -0,0 +1,7 @@
+from hashmap import HashMap
+
+def main():
+    packages = HashMap(40)
+
+
+main()
