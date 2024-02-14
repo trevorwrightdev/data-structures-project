bb9db93d339a78da8aa5aacfddd394379b30f263
trevdev <trevorwrightdev@gmail.com>
2024-02-14 13:07:28 -0800

    added a colored_output function

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ from hashmap import HashMap
 from package import Package
 import csv
 
+def colored_output(color, message):
+    colors = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
+        'magenta': '\033[95m',
+        'cyan': '\033[96m',
+        'white': '\033[97m',
+        'black': '\033[90m',
+        'bright_red': '\033[91;1m',
+        'bright_green': '\033[92;1m',
+        'bright_yellow': '\033[93;1m',
+        'bright_blue': '\033[94;1m',
+        'bright_magenta': '\033[95;1m',
+        'bright_cyan': '\033[96;1m',
+        'bright_white': '\033[97;1m',
+        'reset': '\033[0m'  
+    }
+    
+    if color in colors:
+        print(colors[color] + message + colors['reset'])
+    else:
+        print("Invalid color specified.")
+
 def get_package_csv():
     data = []
     with open('packages.csv', 'r', newline='') as csvfile:
@@ -41,7 +66,7 @@ def get_package_map():
         zipcode = int(package['zip'])
         weight = int(package['weight'])
         notes = package['notes']
-        new_package = Package(package_id, delivery_address, deadline, city, zipcode, weight, notes)
+        new_package = Package(package_id, delivery_address, deadline, city, zipcode, weight)
         map.insert(package_id, new_package)
     return map
 
@@ -63,6 +88,12 @@ def get_distance_between(address1, address2):
     return distance
 
 def main():
+    print('\n')
+    colored_output('cyan', 'Welcome to the WGUPS package delivery system! What would you like to do?')
+    print('\n')
+    colored_output('green', 's - Begin pPackage Delivery Simulation and Package Lookup')
+    colored_output('green', 'q - Quit')
+    print('\n')
     pass
 
 main()
